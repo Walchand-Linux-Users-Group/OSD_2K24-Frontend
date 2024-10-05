5e787c6dfb22cf099a2f4f2173f1b15e98546443
Pranavg1203 <pranavg2016@gmail.com>
2024-10-05 23:06:13 +0530

    rendered beam

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,22 @@
-"use client";;
+"use client";
 import { AnimatedBeam } from "@/components/ui/animated-beam";
 import Confetti from "@/components/ui/confetti";
 import IconCloud from "@/components/ui/icon-cloud";
+import ParentComponent from "@/components/ui/ParentComponent";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <>
-    <h1>Home Page</h1>
-    {/* <AnimatedBeam /> */}
-    <Confetti />
-    <IconCloud />
-   </>
+    <>
+      <h1>home page</h1>
+      <br/>
+      <br/>
+      <br/>
+      <ParentComponent />
+    
+    {/* <Confetti /> */}
+    {/* <IconCloud /> */}
+    </>
   );
 }
+
