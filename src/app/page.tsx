caea3d9ea99373a867b8a4e4ecdc7bfcb3cfe058
Pranavg1203 <pranavg2016@gmail.com>
2024-10-09 23:27:51 +0530

    models

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 import MainOSD from "../pages/MainOSD";
 import AboutUS from "../pages/AboutUS";
-import Git from "../pages/Git"
+import Git from "../pages/Git";
 import Github from "../pages/Github";
-import CICD from "../pages/CICD"
+import CICD from "../pages/CICD";
 import Register from "../pages/Register";
 import { AnimatedBeamDemo } from "@/components/ui/Beam";
-
+import { BorderBeamDemo } from "@/components/ui/lightcard";
 
 export default function Home() {
   return (
     <div>
       OSD
-      <div>
+      <div className="justify-center">
         {/* <AnimatedBeamDemo /> */}
         <BorderBeamDemo />
       </div>
