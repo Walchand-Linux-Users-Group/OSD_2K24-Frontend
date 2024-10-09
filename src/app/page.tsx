a7730401e5945629bd6ce753892ba1289683f38c
Voodels <vighneshpotdar@gmail.com>
2024-10-10 00:13:17 +0530

    git layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
 import MainOSD from "../pages/MainOSD";
 import AboutUS from "../pages/AboutUS";
-import Git from "../pages/Git"
+import Git from "../pages/Git";
 import Github from "../pages/Github";
-import CICD from "../pages/CICD"
+import CICD from "../pages/CICD";
 import Register from "../pages/Register";
 import { AnimatedBeamDemo } from "@/components/ui/Beam";
-
+import { BorderBeamDemo } from "@/components/ui/lightcard";
 
 export default function Home() {
   return (
     <div>
       OSD
       <div>
-        {/* <AnimatedBeamDemo /> */}
-        <BorderBeamDemo />
+        <Git />
       </div>
     </div>
   );
--- src/components/ui/lightcard.tsx
+++ src/components/ui/lightcard.tsx
@@ -2,10 +2,16 @@ import { BorderBeam } from "@/components/ui/border-beam";
 
 export function BorderBeamDemo() {
   return (
-    <div className="relative flex h-[200px] w-[400px] items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+    <div className="relative flex h-[150px] w-[300px] items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
       <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-4xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
-        Border Beam
+        Session 1
       </span>
+      <div className=" p-3 m-3">
+        <p>Topic 1</p>
+        <p>Topic 2</p>
+        <p>Topic 3</p>
+        <p>Topic 4</p>
+      </div>
       <BorderBeam size={30} duration={12} delay={9} />
     </div>
   );
