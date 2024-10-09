315618a2b35b555291872873a046f1065d85a65f
thundersp <thundersp20504@gmail.com>
2024-10-09 20:05:56 +0530

    Added icon cloud

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,10 +1,43 @@
+import IconCloud from "@/components/ui/icon-cloud";
 import React from "react";
 
 const Register = () => {
   return (
-    <div className=" h-screen w-full">
-      <div className="h-full w-full  flex justify-center items-center ">
-        <h1 className=" text-9xl">Register Page</h1>
+    <div className="h-screen w-full">
+      <div className="h-full w-full flex justify-center items-center">
+        <IconCloud
+          iconSlugs={[
+            "typescript",
+            "javascript",
+            "react",
+            "html5",
+            "css3",
+            "ubuntu",
+            "archlinux",
+            "linuxmint",
+            "fedora",
+            "debian",
+            "redhat",
+            "centos",
+            "nodedotjs",
+            "express",
+            "nextdotjs",
+            "kubernetes",  // Kubernetes
+            "amazonaws",   // AWS
+            "terraform",   // Terraform
+            "nginx",
+            "docker",      // Docker
+            "git",         // Git
+            "jira",
+            "github",      // GitHub
+            "gitlab",
+            "visualstudiocode",
+            "sonarqube",   // SonarQube
+            "jenkins",     // Jenkins
+            "jfrog",       // JFrog
+            "eks"          // Amazon EKS (ensure this slug is correct or supported)
+          ]}
+        />
       </div>
     </div>
   );
