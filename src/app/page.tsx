c2018a957d6fc91be6e1a69558db62983c22d56d
Voodels <vighneshpotdar@gmail.com>
2024-10-12 11:58:46 +0530

    dont change app/layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,15 @@ import Git from "../pages/Git";
 import Github from "../pages/Github";
 import CICD from "../pages/CICD";
 import Register from "../pages/Register";
-import { AnimatedBeamDemo } from "@/components/ui/Beam";
-import { BorderBeamDemo } from "@/components/ui/lightcard";
-import Gooey from "@/components/sunnysundown/Gooey";
-import MovingText from "@/components/sunnysundown/Movingtext";
-import Loader from "@/components/sunnysundown/landing";
-import ScratchOffVideo from "@/components/sunnysundown/scratchoffvideo";
 
 export default function Home() {
   return (
     <div>
-      {/* <Loader /> */}
-      {/* <ScratchOffVideo /> */}
-      {/* <MovingText /> */}
-      {/* <Gooey /> */}
+      <MainOSD />
+      <AboutUS />
+      <Git />
+      <Github />
+      <CICD />
       <Register />
     </div>
   );
